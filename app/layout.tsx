7a1dc7e22123db7f092ae6170017aaff58f11824
turikhay <git@turikhay.com>
2023-03-03 03:47:10 +0300

    enable analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Analytics from "./components/analytics";
+
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,7 +9,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
