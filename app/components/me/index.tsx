f009dd8df556942ad397de689496b64b3e9727b2
turikhay <git@turikhay.com>
2025-05-01 19:35:48 +0500

    change pfp and make it expandable

--- app/components/me/index.tsx
+++ app/components/me/index.tsx
@@ -1,30 +1,47 @@
-import { HTMLProps } from "react";
+"use client";
+
+import { HTMLProps, useRef, useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 
 import styles from "./index.module.css";
 import { AGE } from "./age";
 import Name from "./name";
 
-import yuki from "./yuki.png";
+import photo from "./P1020667-crop.jpg";
 
-const yukiDescription = "Yuki (my profile picture)";
-const imageSize = 64;
+const photoAlt = "Picture of Artur Khusainov";
 
 export default function Me(props: HTMLProps<HTMLDivElement>) {
+  const [expanded, setExpanded] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
   return (
-    <div {...props} className={`${props.className ?? ""} ${styles.me}`}>
-      <Link href={yuki.src} target="_blank" title={yukiDescription}>
+    <div
+      {...props}
+      className={`${props.className ?? ""} ${styles.me} ${styles.animated} ${
+        expanded ? styles.expanded : ""
+      }`}
+    >
+      <button
+        className={`${styles.pfp_button} ${styles.animated}`}
+        onClick={() => {
+          setExpanded(!expanded);
+          if (ref.current) {
+            const classList = ref.current.classList;
+            if (!expanded) {
+              classList.remove(styles.collapsed);
+            } else {
+              classList.add(styles.collapsed);
+            }
+          }
+        }}
+      >
         <Image
-          src={yuki}
-          width={imageSize}
-          height={imageSize}
-          className={`${styles.pfp}`}
-          alt={yukiDescription}
-          style={{ borderRadius: `${Math.round(imageSize / 2)}px` }}
+          src={photo}
+          className={`${styles.pfp} ${styles.animated}`}
+          alt={photoAlt}
         />
-      </Link>
-      <div className={`${styles.text}`}>
+      </button>
+      <div className={`${styles.text}`} ref={ref}>
         <Name />
         <div className={styles.subtitle}>
           aka @turikhay (he/him), full stack developer, {AGE} years old
