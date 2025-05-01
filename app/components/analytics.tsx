705f00f6662eeb31df3c7d7bb0e66f10412d87b8
turikhay <git@turikhay.com>
2025-05-01 18:00:34 +0500

    Add SpeedInsights

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function AnalyticsWrapper() {
-  return <Analytics />;
+  return (
+    <>
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
